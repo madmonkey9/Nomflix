ca5bcbf18ced20dca0aebf54ccd4d4cc7f3d86b5
madmonkey <madmonkey3822@gmail.com>
2019-07-09 17:43:40 +0900

    Updates

---
+++ precache-manifest.768011ed0223235a3844b8787b37d460.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dbf03c803d7b5b85cd9c1d86e3a7d94",
+    "url": "/Nomflix/index.html"
+  },
+  {
+    "revision": "ea036a1271d45f9bf7cf",
+    "url": "/Nomflix/static/js/2.d48c9d9a.chunk.js"
+  },
+  {
+    "revision": "0dd90e5fc1e5824a1a8e",
+    "url": "/Nomflix/static/js/main.249aad86.chunk.js"
+  },
+  {
+    "revision": "eb8cc0592254a5d4ede5",
+    "url": "/Nomflix/static/js/runtime~main.18597ffd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nomflix/precache-manifest.91c6b216cd7ea7d94f2309ff05441d37.js"
+  "/Nomflix/precache-manifest.768011ed0223235a3844b8787b37d460.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nomflix/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nomflix/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
